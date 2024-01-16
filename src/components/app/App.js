87234d4ab3a06f887356554c856f3f5bb232ab53
Vladyslav Vuzh <vuzhvlad@Vladyslavs-Laptop.local>
2024-01-16 11:03:57 +0100

    adding React.lazy to all pages to optimize my project, learned lazy() Suspense fallback dynamic import

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,14 @@
+import {lazy, Suspense} from 'react';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
-import { ComicsPage, MainPage, Page404, SingleComicPage } from "../pages"; // it will be looking for index.js that is inside of this folder
-
+// import { ComicsPage, MainPage, SingleComicPage } from "../pages"; // it will be looking for index.js that is inside of this folder 
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from '../spinner/PicSpinner';
+
+const Page404 = lazy(() => import('../pages/404')); // react lazy, will import this page only when we need it
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 
 
 const App = () => {
@@ -12,12 +18,14 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Routes>
-                    <Route path="/" element={<MainPage/>}/>
-                    <Route path="/comics" element={<ComicsPage/>}/>
-                    <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
-                    <Route path="*" element={<Page404/>}/>
-                    </Routes>
+                    <Suspense fallback={<Spinner/>}>
+                        <Routes>
+                            <Route path="/" element={<MainPage/>}/>
+                            <Route path="/comics" element={<ComicsPage/>}/>
+                            <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+                            <Route path="*" element={<Page404/>}/>
+                        </Routes>
+                    </Suspense>
                 </main>
             </div>
         </Router>
